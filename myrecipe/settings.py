6596b48a6e735ca4221056e9b936bc0a7f72d571
Dindihub <sadindi03@gmail.com>
2025-04-06 21:09:42 +0300

    fix bugs

--- myrecipe/settings.py
+++ myrecipe/settings.py
@@ -34,15 +34,7 @@ SECRET_KEY = env.str('SECRET_KEY', default=get_random_secret_key())
 # DEBUG flag
 DEBUG = env.bool('DEBUG', default=False)
 
-# Set ALLOWED_HOSTS based on the environment
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'recipe-home.fly.dev']
-CSRF_TRUSTED_ORIGINS = ['https://fly.io/apps/recipe-home']
-
-# Database settings
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
-
+#Development
 if MODE == "dev":
     DATABASES = {
         'default': {
@@ -60,3 +52,191 @@ else:
             default=env.str('DATABASE_URL')
         )
     }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Set ALLOWED_HOSTS based on the environment
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'recipe-home.fly.dev']
+CSRF_TRUSTED_ORIGINS = ['https://fly.io/apps/recipe-home']
+
+# Application definition
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+    'recipeapp',
+    'cloudinary',
+    'bootstrap4',
+    'crispy_forms',
+    'crispy_bootstrap4',
+
+
+
+
+]
+CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap4"]
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
+]
+
+
+ROOT_URLCONF = 'myrecipe.urls'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+WSGI_APPLICATION = 'myrecipe.wsgi.application'
+
+
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+
+LANGUAGE_CODE = 'en-us'
+
+
+TIME_ZONE = 'Africa/Nairobi'
+
+
+USE_I18N = True
+
+
+USE_TZ = True
+
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # <-- Updated!
+STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Where your actual static files live
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <-- Updated!
+
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+LOGIN_REDIRECT_URL= 'home'
+LOGOUT_REDIRECT_URL= 'home'
+
+
+
+
+
+
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+
+
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+
+
